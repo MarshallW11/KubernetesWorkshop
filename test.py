c5d5d0d32029035baf4cf13d6469e8c7f32e4ae5
Marshall Williams <marshall.leviwilliams@gmail.com>
2025-12-11 21:41:05 -0500

    Kind and kubernetes code

---
+++ test.py
@@ -0,0 +1,15 @@
+from requests import post
+
+url = 'http://localhost:8080/predict'
+
+request = {
+    "url": "http://bit.ly/mlbookcamp-pants"
+}
+
+response = post(url, json=request)
+result = response.json()
+
+print(f"Top prediction: {result['top_class']} ({result['top_probability']:.2%})")
+print(f"\nAll predictions:")
+for cls, prob in result['predictions'].items():
+    print(f"  {cls:12s}: {prob:.2%}")
